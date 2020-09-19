78999aa8082ff342f823d040cd430a1c3cab6859
Christian Blötner <c.bloetner@gmail.com>
2020-09-19 21:20:20 +0000

    version 0.4.0

--- R/source_code_diffcor.R
+++ R/source_code_diffcor.R
@@ -14,15 +14,15 @@ diffcor.one <- function(emp.r, hypo.r, n, cor.names = NULL, alternative = c("one
   exp.r <- atanh(hypo.r)+(hypo.r/((2*n)-1));
   Var.z <- (n-3);
 
-  diff.z.one <- (fisher.emp-exp.r)*sqrt(Var.z);
-  Cohen.q.one <- fisher.emp-exp.r;
+  diff.z.one <- round((fisher.emp-exp.r)*sqrt(Var.z), 2);
+  Cohen.q.one <- round((fisher.emp-exp.r), 2);
 
   alternative <- match.arg(alternative);
   tside <- switch(alternative, one.sided = 1, two.sided = 2)
   if (tside == 1)
-    p <- 1-pnorm(abs(diff.z.one));
+    p <- format(round(1-pnorm(abs(diff.z.one)), 4), scientific = F);
   if (tside == 2)
-    p <- 2*pnorm(-abs(diff.z.one));
+    p <- format(round(2*pnorm(-abs(diff.z.one)), 4), scientific = F);
 
   res.one <- data.frame(diff.z.one, p, Cohen.q.one);
   colnames(res.one) <- c("Fisher z", "p value", "Cohen's q");
@@ -45,15 +45,15 @@ diffcor.two <- function(r1, r2, n1, n2, cor.names = NULL, alternative = c("one.s
   fisher1 <- atanh(r1); fisher2 <- atanh(r2);
   Var1 <- 1/(n1-3); Var2 <- 1/(n2-3); SE <- sqrt((Var1+Var2));
 
-  diff.z.two <- (fisher1-fisher2)/SE;
-  Cohen.q.two <- (fisher1-fisher2)
+  diff.z.two <- round(((fisher1-fisher2)/SE), 2);
+  Cohen.q.two <- round((fisher1-fisher2), 2)
 
   alternative <- match.arg(alternative);
   tside <- switch(alternative, one.sided = 1, two.sided = 2)
   if (tside == 1)
-    p <- 1-pnorm(abs(diff.z.two));
+    p <- format(round(1-pnorm(abs(diff.z.two)), 4), scientific = F);
   if (tside == 2)
-    p <- 2*pnorm(-abs(diff.z.two));
+    p <- format(round(2*pnorm(-abs(diff.z.two)), 4), scientific = F);
 
 
   res.two <- data.frame(diff.z.two, p, Cohen.q.two);
@@ -79,14 +79,14 @@ diffcor.dep <- function(r12, r13, r23, n, cor.names = NULL, alternative = c("one
   Cov.dep.a <- 1/(1-r1^2)^2; Cov.dep.b <- r23*(1-2*r1^2); Cov.dep.c <- -.50*(r1^2); Cov.dep.d <- 1-(2*(r1^2))-r23^2;
   Cov.dep <- (Cov.dep.a*Cov.dep.b)+(Cov.dep.c*Cov.dep.d);
   SE.dep <- sqrt((2-(2*Cov.dep))/(n-3));
-  diff.z.dep <- (z12-z13)/SE.dep;
+  diff.z.dep <- round(((z12-z13)/SE.dep), 2);
 
   alternative <- match.arg(alternative);
   tside <- switch(alternative, one.sided = 1, two.sided = 2)
   if (tside == 1)
-    p <- 1-pnorm(abs(diff.z.dep));
+    p <- format(round(1-pnorm(abs(diff.z.dep)), 4), scientific = F);
   if (tside == 2)
-    p <- 2*pnorm(-abs(diff.z.dep));
+    p <- format(round(2*pnorm(-abs(diff.z.dep)), 4), scientific = F);
 
 
   res.dep <- data.frame(diff.z.dep, p);
