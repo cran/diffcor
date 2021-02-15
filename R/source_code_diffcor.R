14bdc95c240b87c9ea42dfb333e78ebcbbb06e87
Christian Blötner <c.bloetner@gmail.com>
2021-02-15 08:20:03 +0000

    version 0.6.4

--- R/source_code_diffcor.R
+++ R/source_code_diffcor.R
@@ -5,36 +5,34 @@
 #' @param emp.r empirically observed correlation
 #' @param hypo.r expected correlation
 #' @param n sample size the observed correlation is based on
-#' @return Fisher's z-values, corresponding p-values, adjusted alpha level, and Cohen's q
+#' @return Fisher's z-values, confidence intervals, corresponding p-values, and Cohen's q
 #' @export
 
-diffcor.one <- function(emp.r, hypo.r, n, cor.names = NULL, alternative = c("one.sided", "two.sided"), bonferroni = FALSE, digit = 3, alpha = .05){
-  fisher.emp <- atanh(emp.r);
-  exp.r <- atanh(hypo.r)+(hypo.r/((2*n)-1));
-  Var.z <- (n-3);
+diffcor.one <- function(emp.r, hypo.r, n, cor.names = NULL,
+                        alternative = c("one.sided", "two.sided"), digit = 3){
 
-  diff.z.one <- round((fisher.emp-exp.r)*sqrt(Var.z), 2);
-  Cohen.q.one <- round((fisher.emp-exp.r), 2);
+  fisher.emp <- atanh(emp.r);
+  exp.r <- atanh(hypo.r) + (hypo.r / ((2 * n) - 1));
+  Var.z <- (n - 3);
+
+  diff.z.one <- round((fisher.emp - exp.r) * sqrt(Var.z), 2);
+  Cohen.q.one <- round((fisher.emp - exp.r), 2);
 
   alternative <- match.arg(alternative);
   tside <- switch(alternative, one.sided = 1, two.sided = 2)
+
   if (tside == 1){
-    p <- format(round(1-pnorm(abs(diff.z.one)), digit), scientific = F)};
+    p <- format(round(1 - pnorm(abs(diff.z.one)), digit), scientific = F)}
+
   if (tside == 2){
-    p <- format(round(2*pnorm(-abs(diff.z.one)), digit), scientific = F)};
+    p <- format(round(2 * pnorm(-abs(diff.z.one)), digit), scientific = F)};
 
-  alpha.Bonferroni <- round(alpha/length(fisher.emp), digit);
-  significant <- p < alpha.Bonferroni;
+  LL.z <- fisher.emp - (1.96 / sqrt(Var.z)); LL <- round(tanh(LL.z), digit)
+  UL.z <- fisher.emp + (1.96 / sqrt(Var.z)); UL <- round(tanh(UL.z), digit);
 
-  if(bonferroni == TRUE){
-    res.one <- data.frame("Fisher_z" = diff.z.one, "p" = p,
-                          "alpha_Bonferroni" = alpha.Bonferroni,
-                          "Bonferroni_significant" = significant,
-                          "Cohen_q" = Cohen.q.one)};
-  if(bonferroni == FALSE){
-    res.one <- data.frame("Fisher_z" = diff.z.one, "p" = p,
-                          "Cohen_q" = Cohen.q.one)};
-  row.names(res.one) <- cor.names;
+  res.one <- data.frame(diff.z.one, LL, UL, p, Cohen.q.one);
+  rownames(res.one) <- cor.names;
+  colnames(res.one) <- c("z", "LL", "UL", "p", "Cohen_q");
 
   return(res.one)}
 
@@ -48,32 +46,32 @@ diffcor.one <- function(emp.r, hypo.r, n, cor.names = NULL, alternative = c("one
 #' @return Fisher's z-value, corresponding p-values, and Cohen's q
 #' @export
 
-diffcor.two <- function(r1, r2, n1, n2, cor.names = NULL, alternative = c("one.sided", "two.sided"), bonferroni = FALSE, digit = 3, alpha = .05){
-  fisher1 <- atanh(r1); fisher2 <- atanh(r2);
-  Var1 <- 1/(n1-3); Var2 <- 1/(n2-3); SE <- sqrt((Var1+Var2));
+diffcor.two <- function(r1, r2, n1, n2, cor.names = NULL,
+                        alternative = c("one.sided", "two.sided"), digit = 3){
 
-  diff.z.two <- round(((fisher1-fisher2)/SE), 2);
-  Cohen.q.two <- round((fisher1-fisher2), 2)
+  fisher1 <- atanh(r1); fisher2 <- atanh(r2);
+  Var1 <- 1/(n1 - 3); Var2 <- 1/(n2 - 3); SE <- sqrt((Var1 + Var2));
+
+  diff.z.two <- round(((fisher1 - fisher2) / SE), 2);
+  Cohen.q.two <- round((fisher1 - fisher2), 2)
 
   alternative <- match.arg(alternative);
   tside <- switch(alternative, one.sided = 1, two.sided = 2)
   if (tside == 1){
-    p <- format(round(1-pnorm(abs(diff.z.two)), digit), scientific = F)};
+    p <- format(round(1 - pnorm(abs(diff.z.two)), digit), scientific = F)};
+
   if (tside == 2){
-    p <- format(round(2*pnorm(-abs(diff.z.two)), digit), scientific = F)};
+    p <- format(round(2 * pnorm(-abs(diff.z.two)), digit), scientific = F)};
 
-  alpha.Bonferroni <- round(alpha/length(fisher1), digit);
-  significant <- p < alpha.Bonferroni;
+  LL.z.1 <- fisher1 - (1.96 * Var1); LL1 <- round(tanh(LL.z.1), digit);
+  UL.z.1 <- fisher1 + (1.96 * Var1); UL1 <- round(tanh(UL.z.1), digit);
 
-  if(bonferroni == TRUE){
-    res.two <- data.frame("Fisher_z" = diff.z.two, "p" = p,
-                          "alpha_Bonferroni" = alpha.Bonferroni,
-                          "Bonferroni_significant" = significant,
-                          "Cohen_q" = Cohen.q.two)};
-  if(bonferroni == FALSE){
-    res.two <- data.frame("Fisher_z" = diff.z.two, "p" = p,
-                          "Cohen_q" = Cohen.q.two)};
-  row.names(res.two) <- cor.names;
+  LL.z.2 <- fisher2 - (1.96 * Var2); LL2 <- round(tanh(LL.z.2), digit);
+  UL.z.2 <- fisher2 + (1.96 * Var2); UL2 <- round(tanh(UL.z.2), digit);
+
+  res.two <- data.frame(diff.z.two, LL1, UL1, LL2, UL2, p, Cohen.q.two)
+  rownames(res.two) <- cor.names;
+  colnames(res.two) <- c("z", "LL1", "UL1", "LL2", "UL2", "p", "Cohen_q")
 
   return(res.two)
   }
@@ -88,29 +86,26 @@ diffcor.two <- function(r1, r2, n1, n2, cor.names = NULL, alternative = c("one.s
 #' @return Fisher's z-value and corresponding p-values
 #' @export
 
-diffcor.dep <- function(r12, r13, r23, n, cor.names = NULL, alternative = c("one.sided", "two.sided"), bonferroni = FALSE, digit = 3, alpha = .05){
-  z12 <- atanh(r12); z13 <- atanh(r13); r1 <- (r12+r13)/2;
-  Cov.dep.a <- 1/(1-r1^2)^2; Cov.dep.b <- r23*(1-2*r1^2); Cov.dep.c <- -.50*(r1^2); Cov.dep.d <- 1-(2*(r1^2))-r23^2;
-  Cov.dep <- (Cov.dep.a*Cov.dep.b)+(Cov.dep.c*Cov.dep.d);
-  SE.dep <- sqrt((2-(2*Cov.dep))/(n-3));
-  diff.z.dep <- round(((z12-z13)/SE.dep), 2);
+diffcor.dep <- function(r12, r13, r23, n, cor.names = NULL,
+                        alternative = c("one.sided", "two.sided"), digit = 3){
+
+  z12 <- atanh(r12); z13 <- atanh(r13); r1 <- (r12 + r13) / 2;
+  Cov.dep.a <- 1 / (1 - r1^2)^2; Cov.dep.b <- r23 * (1 - 2 * r1^2)
+  Cov.dep.c <- -.50 * (r1^2); Cov.dep.d <- 1 - (2 * (r1^2)) - r23^2;
+  Cov.dep <- (Cov.dep.a * Cov.dep.b) + (Cov.dep.c * Cov.dep.d);
+  SE.dep <- sqrt((2 - (2 * Cov.dep)) / (n - 3));
+  diff.z.dep <- round(((z12 - z13) / SE.dep), 2);
 
   alternative <- match.arg(alternative);
   tside <- switch(alternative, one.sided = 1, two.sided = 2)
   if (tside == 1){
-    p <- format(round(1-pnorm(abs(diff.z.dep)), digit), scientific = F)};
+    p <- format(round(1 - pnorm(abs(diff.z.dep)), digit), scientific = F)};
   if (tside == 2){
-    p <- format(round(2*pnorm(-abs(diff.z.dep)), digit), scientific = F)};
+    p <- format(round(2 * pnorm(-abs(diff.z.dep)), digit), scientific = F)};
 
-  alpha.Bonferroni <- round(alpha/length(z12), digit);
-  significant <- p < alpha.Bonferroni;
+  res.dep <- data.frame(diff.z.dep, p);
+  rownames(res.dep) <- cor.names;
+  colnames(res.dep) <- c("z", "p");
 
-  if(bonferroni == TRUE){
-    res.dep <- data.frame("Fisher_z" = diff.z.dep, "p" = p,
-                          "alpha_Bonferroni" = alpha.Bonferroni,
-                          "Bonferroni_significant" = significant)};
-  if(bonferroni == FALSE){
-    res.dep <- data.frame("Fisher_z" = diff.z.dep, "p" = p)};
-  row.names(res.dep) <- cor.names;
   return(res.dep)
 }
